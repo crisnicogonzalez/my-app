0a1a89fac0c97e65a566b6f3bb47fce555df6cac
crisnicogonzalez <cristian3629@gmail.com>
2018-05-28 01:26:50 -0300

    Define other theme and define appbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,28 +39,8 @@ class Index extends Component {
     const { open } = this.state;
 
     return (
-      <div className={classes.root}>
+      <div>
         <AppBar />
-        <Dialog open={open} onClose={this.handleClose}>
-          <DialogTitle>Super Secret Password</DialogTitle>
-          <DialogContent>
-            <DialogContentText>1-2-3-4-5</DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button color="primary" onClick={this.handleClose}>
-              OK
-            </Button>
-          </DialogActions>
-        </Dialog>
-        <Typography variant="display1" gutterBottom>
-          Material-UI
-        </Typography>
-        <Typography variant="subheading" gutterBottom>
-          example project
-        </Typography>
-        <Button variant="raised" color="secondary" onClick={this.handleClick}>
-          Super Secret Password
-        </Button>
       </div>
     );
   }
